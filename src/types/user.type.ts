c63be4f385f0230b412322df1c0f1989f2a57b42
xuannam59 <167595987+xuannam59@users.noreply.github.com>
2026-09-17 17:55:45 +0700

    Need to deploy production

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -5,3 +5,6 @@ console.log(123);
 console.log(123);
 console.log('1');
 console.log('ready to deployed');
+console.log('still need confirmation');
+console.log('still pending');
+console.log('Need to deploy production');
